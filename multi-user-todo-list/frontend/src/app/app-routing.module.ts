36a9c861471da642141e537ff2426eb2b3ff3d1f
Loïc Poullain <loic.poullain@centraliens.net>
2019-01-28 08:34:26 +0100

    Add Guard

--- multi-user-todo-list/frontend/src/app/app-routing.module.ts
+++ multi-user-todo-list/frontend/src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { AuthGuardService } from './services/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, canActivate: [ AuthGuardService ] },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
